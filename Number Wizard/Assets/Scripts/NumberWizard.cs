a06de687e44718f1c3d793ce107524ca0f43d32a
Timothy Perez <tcperez1993@gmail.com>
2019-09-30 22:10:40 -0600

    Number Wizard Prep

---
+++ Number Wizard/Assets/Scripts/NumberWizard.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NumberWizard : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("Welcome to Number Wizard: Console Edition!");
+        Debug.Log("Pick a number, any number!");
+        Debug.Log("Not too high though, we can only count to 1000...");
+        Debug.Log("Heck, you could even start at 1!"); 
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
