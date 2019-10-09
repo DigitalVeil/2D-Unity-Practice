aba4ffb51f3ea1c8b161d8e5d5b687dbf934c5bc
Timothy Perez <tcperez1993@gmail.com>
2019-10-08 22:25:44 -0600

    Text Adventure UI/Script Work

---
+++ Playing With Sprites/Assets/Scripts/TextGame.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TextGame : MonoBehaviour
+{
+    [SerializeField] Text textComponent; 
+    // Start is called before the first frame update
+    void Start()
+    {
+        textComponent.text = ("When humanity is at its end, what will your last moments be? ");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
